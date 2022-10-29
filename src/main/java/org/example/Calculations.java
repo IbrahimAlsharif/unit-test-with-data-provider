9211878809829dbf168b8ac8fbe7b875533d8566
ibrahim <isharif@famcare.app>
2022-10-29 12:30:54 +0200

    correct factorial function set initial factorial value to 1

--- src/main/java/org/example/Calculations.java
+++ src/main/java/org/example/Calculations.java
@@ -5,7 +5,7 @@ public class Calculations {
     // but it's not included here, because this implementation is made for sake of
     // explaining the unit tests' usage only
     public int factorial(int number){
-        int factorial =2;
+        int factorial =1;
         while (number >1){
             factorial = factorial * number;
             number--;
