964708ffb8b6ab2a9bc2720fd7534a389a8254df
ibrahim <isharif@famcare.app>
2022-10-29 12:25:07 +0200

    update factorial function

--- src/main/java/org/example/Calculations.java
+++ src/main/java/org/example/Calculations.java
@@ -5,7 +5,7 @@ public class Calculations {
     // but it's not included here, because this implementation is made for sake of
     // explaining the unit tests' usage only
     public int factorial(int number){
-        int factorial =1;
+        int factorial =2;
         while (number >1){
             factorial = factorial * number;
             number--;
